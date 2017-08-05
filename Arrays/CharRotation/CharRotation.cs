975722348b4e364196cb5859a4e51b7953076966
AnaKostadinova <ana.k.kostadinova@gmail.com>
2017-08-05 23:46:37 +0300

    ...

---
+++ Arrays/CharRotation/CharRotation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+
+namespace CharRotation
+{
+    public class CharRotation
+    {
+        public static void Main()
+        {
+            string symbols = Console.ReadLine();
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string result = string.Empty;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    result += (char)(symbols[i] - numbers[i]);
+                }
+                else
+                {
+                    result += (char)(symbols[i] + numbers[i]);
+                }
+            }
+            Console.WriteLine(result);
+        }        
+        }
+    }
+
